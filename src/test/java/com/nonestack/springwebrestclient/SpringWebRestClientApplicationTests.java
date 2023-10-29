d1f8a65c3d9c1365dd1629b05f363a3b319148b2
djamelkorei <korei.djamel.eddine@gmail.com>
2023-10-29 20:49:28 +0000

    initial commit

---
+++ src/test/java/com/nonestack/springwebrestclient/SpringWebRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonestack.springwebrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
